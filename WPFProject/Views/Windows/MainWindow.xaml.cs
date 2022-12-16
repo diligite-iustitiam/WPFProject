2b78b25241884ded2a709022b8365fef8d5f0438
diligite-iustitiam <107812456+diligite-iustitiam@users.noreply.github.com>
2022-12-16 15:51:46 +0200

    Infrastructure of project folders

---
+++ WPFProject/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Windows;
+
+namespace WPFProject
+{  
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
