c20c0e2d1c701030fb174bcf2567ccbd07d4b2ef
diligite-iustitiam <107812456+diligite-iustitiam@users.noreply.github.com>
2023-01-02 18:34:41 +0200

    Filters

--- WPFProject/Views/Windows/MainWindow.xaml.cs
+++ WPFProject/Views/Windows/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using WPFProject.Models.Decanat;
 
 namespace WPFProject
 {  
@@ -8,6 +11,28 @@ namespace WPFProject
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GroupsCollection_Filter(object sender, System.Windows.Data.FilterEventArgs e)
+        {
+            if (!(e.Item is Group group)) return;
+            if(group.GroupName is null) return;
+
+            var filter_text = GroupNameFilterText.Text;
+            if(filter_text.Length == 0) return;
+
+            if (group.GroupName.Contains(filter_text, System.StringComparison.OrdinalIgnoreCase));
+            if(group.Description != null &&group.Description.Contains(filter_text, System.StringComparison.OrdinalIgnoreCase));
+
+            e.Accepted = false;
+
+        }
+
+        private void OnGroupsFilterTextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            var text_box = (TextBox)sender;
+            var collection = (CollectionViewSource)text_box.FindResource("GroupsCollection");
+            collection.View.Refresh();
         }
     }
 }
